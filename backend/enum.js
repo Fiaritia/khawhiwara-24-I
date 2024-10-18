56f0af4417b135cc4350c5e557831a9118c96776
Fiaritia <rahamisonf@gmail.com>
2024-10-18 14:55:31 +0300

    Création des enums.

---
+++ backend/enum.js
@@ -0,0 +1,21 @@
+const Sampana = Object.freeze({
+    MAVO: 'MAVO',
+    MAITSO: 'MAITSO',
+    MENA: 'MENA',
+    MENAFIFY: 'MENAFIFY'
+});
+
+const Enina = Object.freeze({
+    FOTSY: 'FOTSY',
+    MARA: 'MARA',
+    PENTINA: 'PENTINA',
+    MAINTY: 'MAINTY',
+    MANJA: 'MANJA',
+    NATO: 'NATO',
+    RANOMBALAHY: 'RANOMBALAHY',
+    JAKY: 'JAKY',
+    LALONA: 'LALONA',
+    VATOLALAKA: 'VATOLALAKA'
+});
+
+module.exports = { Sampana, Enina };
